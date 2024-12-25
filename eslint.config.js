7502a4c04908185da33a17db6e799aea987c59a1
Bluzzi <camilledugas.pro@gmail.com>
2024-12-25 20:27:53 +0100

    style(eslint.config.js): standardize quotes for consistency in tsconfigPath

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { eslintConfig } from "./dist/index.js";
 
 export default eslintConfig(
   {
-    typescript: { tsconfigPath: `./tsconfig.json` },
+    typescript: { tsconfigPath: "./tsconfig.json" },
   },
   {
     rules: { "ts/no-explicit-any": "off" },
